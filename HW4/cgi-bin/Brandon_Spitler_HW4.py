42e6c39af2fc40c4645da7f6a7fc44fef09d8537
Brandon Spitler <Brandonfspitler@gmail.com>
2016-06-20 23:14:11 -0600

    done map is orange though

--- HW4/cgi-bin/Brandon_Spitler_HW4.py
+++ HW4/cgi-bin/Brandon_Spitler_HW4.py
@@ -3,6 +3,76 @@ import unicodedata
 import urllib2
 import json
 
+
+#from https://github.com/tdlm/fun-with-python/blob/master/states_and_capitals.py
+
+states_caps = {
+        'AL':{'state':'Alabama',        'capital':'Montgomery'},
+        'AK':{'state':'Alaska',         'capital':'Juneau'},
+        'AZ':{'state':'Arizona',        'capital':'Phoenix'},
+        'AR':{'state':'Arkansas',       'capital':'Little Rock'},
+        'CA':{'state':'California',     'capital':'Sacramento'},
+        'CO':{'state':'Colorado',       'capital':'Denver'},
+        'CT':{'state':'Connecticut',    'capital':'Hartford'},
+        'DE':{'state':'Deleware',       'capital':'Dover'},
+        'FL':{'state':'Florida',        'capital':'Tallahassee'},
+        'GA':{'state':'Georgia',        'capital':'Atlanta'},
+        'HI':{'state':'Hawaii',         'capital':'Honolulu'},
+        'ID':{'state':'Idaho',          'capital':'Boise'},
+        'IL':{'state':'Illinois',       'capital':'Springfield'},
+        'IN':{'state':'Indiana',        'capital':'Indianapolis'},
+        'IA':{'state':'Iowa',           'capital':'Des Moines'},
+        'KS':{'state':'Kansas',         'capital':'Topeka'},
+        'KY':{'state':'Kentucky',       'capital':'Frankfort'},
+        'LA':{'state':'Louisiana',      'capital':'Baton Rouge'},
+        'ME':{'state':'Maine',          'capital':'Augusta'},
+        'MD':{'state':'Maryland',       'capital':'Annapolis'},
+        'MA':{'state':'Massachusetts',  'capital':'Boston'},
+        'MI':{'state':'Michigan',       'capital':'Lansing'},
+        'MN':{'state':'Minnesota',      'capital':'Saint Paul'},
+        'MS':{'state':'Mississippi',    'capital':'Jackson'},
+        'MO':{'state':'Missouri',       'capital':'Jefferson City'},
+        'MT':{'state':'Montana',        'capital':'Helena'},
+        'NE':{'state':'Nebraska',       'capital':'Lincoln'},
+        'NV':{'state':'Nevada',         'capital':'Carson City'},
+        'NH':{'state':'New Hampshire',  'capital':'Concord'},
+        'NJ':{'state':'New Jersey',     'capital':'Trenton'},
+        'NM':{'state':'New Mexico',     'capital':'Santa Fe'},
+        'NY':{'state':'New York',       'capital':'Albany'},
+        'NC':{'state':'North Carolina', 'capital':'Raleigh'},
+        'ND':{'state':'North Dakota',   'capital':'Bismarck'},
+        'OH':{'state':'Ohio',           'capital':'Columbus'},
+        'OK':{'state':'Oklahoma',       'capital':'Oklahoma City'},
+        'OR':{'state':'Oregon',         'capital':'Salem'},
+        'PA':{'state':'Pennsylvania',   'capital':'Harrisburg'},
+        'RI':{'state':'Rhode Island',   'capital':'Providence'},
+        'SC':{'state':'South Carolina', 'capital':'Columbia'},
+        'SD':{'state':'South Dakota',   'capital':'Pierre'},
+        'TN':{'state':'Tennessee',      'capital':'Nashville'},
+        'TX':{'state':'Texas',          'capital':'Austin'},
+        'UT':{'state':'Utah',           'capital':'Salt Lake City'},
+        'VT':{'state':'Vermont',        'capital':'Montpelier'},
+        'VA':{'state':'Virginia',       'capital':'Richmond'},
+        'WA':{'state':'Washington',     'capital':'Olympia'},
+        'WV':{'state':'West Virginia',  'capital':'Charleston'},
+        'WI':{'state':'Wisconsin',      'capital':'Madison'},
+        'WY':{'state':'Wyoming',        'capital':'Cheyenne'},
+    }
+
+def getColor(temp):
+	tempFah=1.8*(temp-273)+32
+	if tempFah<10:
+		return "Blue"
+	elif tempFah<30:
+		return "Cyan"
+	elif tempFah<50:
+		return "Green"
+	elif tempFah<80:
+		return "Orange"
+	else:
+		return "Red"
+	
+
 #function from http://stackoverflow.com/questions/13588697/getting-weather-conditions-using-python-im-confused-by-json-format
 def getWeather(city):
 	try:
@@ -107,16 +177,34 @@ print contents
 data=json.loads(getWeather("Boulder"))
 #data=unicode.normalize('NFKD',title).encode('ascii','ignore')
 data = byteify(data)
-print data
-print'''
-<script>
-$( document ).ready(function() {
-'''
-print "$('#CO').css('fill','red')"
-print '''
-});
+#print data
 
-</script>
-'''
+#print "<br>"
+text=""
+for key in states_caps:
+#	print "<br>"
+#	print str(states_caps[key]['capital'])
+	data=json.loads(getWeather("\""+states_caps[key]['capital']+"\""))
+	#print data
+	data = byteify(data)
+	
+	temp=data['list'][3]['main']['temp']
+	#print temp
+	color=getColor(temp)
+	#print color
+	print '''
+	<script>
+	$( document ).ready(function(){
+	'''
+	print "$(\'#" + key + "\').css(\'fill\',\'" +color+"\')"
+	print '''
 
+
+	});
+	</script>
+	''' 
+	#text+=json.loads(getWeather(states_caps[key]['capital'])
+	#text+="<br>"
+#print "<br>"
+#print text
 import cgi, cgitb
